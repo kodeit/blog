60ba269f888e4eae088bc40f0010a12447c451f2
Shameem <shameemkhd@gmail.com>
2016-11-20 18:38:20 +0530

    Posts: configured posts app admin interface.

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = ["title", "created", "get_category"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
