c9ad58a211cdcf06b95b418dbcddc8032594ca7f
Shameem <shameemkhd@gmail.com>
2016-11-20 20:22:02 +0530

    Post: Added view for sort and search features.

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Q
 from django.views.generic import ListView
 
 from .models import Post
@@ -9,4 +10,24 @@ class PostListView(ListView):
 
     template_name = 'posts/post_list.html'
     context_object_name = "object_list"
-    paginate_by = 1  # and that's it !!
+    paginate_by = 3
+
+    def get_queryset(self):
+
+        queryset = super(PostListView, self).get_queryset()
+
+        search_query = self.request.GET.get("q")
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(description__icontains=search_query) |
+                Q(summary__icontains=search_query)
+            ).distinct()
+
+        sort_query = self.request.GET.get("sort")
+        if sort_query == "most_visited":
+            queryset = queryset.order_by("-visit_count")
+        elif sort_query == "latest":
+            queryset = queryset.order_by("-created")
+
+        return queryset
