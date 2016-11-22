7adca427a8a0857dd2823b860c281a7001be6371
Shameem <shameemkhd@gmail.com>
2016-11-22 21:51:06 +0530

    Templates: Added crispy forms support.

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # third party apps
+    'crispy_forms',
+
+    # local apps
     'posts',
     'accounts',
 ]
