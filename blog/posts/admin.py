82bc67397ccef5ef0db0ada2540978d9c22a2de5
Shameem <shameemkhd@gmail.com>
2016-11-27 13:50:11 +0530

    Posts: Modified posts admin interface to support comments.
    This changeset is to enable comment option in posts model admin interface.

--- blog/posts/admin.py
+++ blog/posts/admin.py
@@ -1,12 +1,25 @@
 from django.contrib import admin
+from django.contrib.contenttypes.admin import GenericTabularInline
 
-from .models import Category, Post
+from comments.models import Comment
+from posts.models import Category, Post
+
+
+class CommentInline(GenericTabularInline):
+    model = Comment
+    '''
+    ct_field = "content_type" # default value is content_type.
+    ct_fk_field = "object_id" # default value is object_id.
+    '''
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ["id", "title", "created", "get_category"]
+    inlines = [
+        CommentInline,
+    ]
 
 
 @admin.register(Category)
