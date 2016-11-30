af6ae0e94cd4feea9b0fcb7696880fd935f23f37
Shameem <shameemkhd@gmail.com>
2016-11-30 15:42:15 +0530

    Reorganized post detail html elements and added some basic style.
    1) Added simple style to comment form.
    2) Adjusted textarea col and row size.

--- blog/comments/forms.py
+++ blog/comments/forms.py
@@ -4,9 +4,10 @@ from .models import Comment
 
 class CommentForm(forms.ModelForm):
 
-    comment = forms.CharField(widget=forms.Textarea)
-
     class Meta:
         model = Comment
-        exclude = ('content_type', 'object_id',
-                   'content_object')
+        exclude = ('content_type', 'object_id', 'content_object')
+
+        widgets = {
+           'comment': forms.Textarea(attrs={'cols': 78, 'rows': 6}),
+        }
